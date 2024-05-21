6b3cd311ef5556ab12f85f6574a0b9d1a5206de1
Martin Chapman <contact@martinchapman.co.uk>
2024-05-21 11:41:16 +0100

    Update made by Phenoflow

---
+++ python/herpesviral-infection-of-anal-and-rectal-regions---secondary.py
@@ -0,0 +1,25 @@
+# Kuan V, Denaxas S, Gonzalez-Izquierdo A, Direk K, Bhatti O, Husain S, Sutaria S, Hingorani M, Nitsch D, Parisinos C, Lumbers T, Mathur R, Sofat R, Casas JP, Wong I, Hemingway H, Hingorani A, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"A60.9","system":"icd10"},{"code":"A60.1","system":"icd10"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('infection-of-anal-and-rectal-regions-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["herpesviral-infection-of-anal-and-rectal-regions---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["herpesviral-infection-of-anal-and-rectal-regions---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["herpesviral-infection-of-anal-and-rectal-regions---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
